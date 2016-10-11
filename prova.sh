446db3737f7421b67fc821e25024f3559c03e7c8
Kleber Leal <kal2@cin.upfe.br>
2016-10-11 06:14:48 -0300

    prova

---
+++ prova.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+date > /root/prova/data.txt
+history > /root/prova/historico.txt
+
+
+tar zxvf /root/prova.tar.gz /root/prova /etc/passwd /etc/shadow
+
+echo "Envie o arquivo prova.tar.gz por e-mail para kleber.leal@palmares.ifpe.edu.br"
