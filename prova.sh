5acc078e4e97f8e24990c8ade60af720d6aaf547
root <root@KLEBER-HP.home>
2016-10-17 15:06:11 -0300

    eba

--- prova.sh
+++ prova.sh
@@ -2,8 +2,9 @@
 
 date > /root/prova/data.txt
 history > /root/prova/historico.txt
+ls -R /root/prova > /root/prova/listagem.txt
 
 
-tar zcvf /root/prova.tar.gz /root/prova /etc/passwd /etc/shadow
+tar zcvf /root/prova.tar.gz /root/prova /etc/passwd /etc/shadow /etc/group
 
 echo "Envie o arquivo prova.tar.gz por e-mail para kleber.leal@palmares.ifpe.edu.br"
